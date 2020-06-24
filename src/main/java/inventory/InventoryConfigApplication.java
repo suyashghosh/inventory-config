7609e53108914bc61ae16c8fe439206009da8858
admin <suyashghosh@gmail.com>
2020-06-24 16:30:49 +0530

    initial

---
+++ src/main/java/inventory/InventoryConfigApplication.java
@@ -0,0 +1,15 @@
+package inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class InventoryConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryConfigApplication.class, args);
+	}
+
+}
